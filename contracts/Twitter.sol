29c0a37b16ffb0b2ce0bd2e9dab7bbe36d141a4f
Harmeet <hs978698@gmail.com>
2024-07-25 23:40:22 +0530

    twitter contract in progress

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -1,3 +1,11 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.26;
+
+contract Twitter{
+    mapping(address => string) public tweets;
+
+    function createTweet(string memory _tweet) public {
+        tweets[msg.sender] = _tweet;
+    }
+}
