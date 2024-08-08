6d7e7f271d38a14d223c81b86e95095afba5c2fc
Harmeet <hs978698@gmail.com>
2024-08-08 23:38:52 +0530

    tweet just had some changes

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -4,23 +4,29 @@ pragma solidity ^0.8.26;
 
 contract Twitter{
 
+    uint8 constant MAX_TWEET_LENGTH = 200;
     struct Tweet{
         address author;
         string content;
         uint256 timestamp;
         uint likes;
+        uint reach;
     }
 
     mapping(address => Tweet[]) public tweets;
 
     function createTweet(string memory _tweet) public {
+
+        require(bytes(_tweet).length <= MAX_TWEET_LENGTH, "Tweet cannot be that big bruh!! ");
         Tweet memory newTweet=Tweet({
             author: msg.sender,
             content: _tweet,
             timestamp: block.timestamp,
-            likes: 0
+            likes: 0,
+            reach: 0
 
         });
+
         tweets[msg.sender].push(newTweet);
     }
 
@@ -31,6 +37,4 @@ contract Twitter{
     function getAllTweets(address _owner) public view returns (Tweet[] memory){
         return tweets[_owner];
     }
-
-    
 }
