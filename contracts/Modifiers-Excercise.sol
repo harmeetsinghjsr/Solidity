f0a5395caf6008e29e40b7f4c7cee6b84e00a434
Harmeet <hs978698@gmail.com>
2024-08-24 17:30:35 +0530

    modifiers

---
+++ contracts/Modifiers-Excercise.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.26;
+
+contract PausableToken {
+    address  public owner;
+    bool public paused;
+    mapping(address => uint) public balances;
+
+    constructor() {
+        owner = msg.sender;
+        paused = false;
+        balances[owner] = 1000;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "YOU ARE NOT THE OWNER");
+        _;
+    }
+
+    modifier notPaused() {
+        require(paused == false, "CONTRACT PAUSED");
+        _;
+    }
+    function pause() public onlyOwner {
+        paused = true;
+    }
+
+    function unpause() public onlyOwner {
+        paused = false;
+    }
+
+    function transfer(address to,uint amount) public notPaused{
+        require(balances[msg.sender] >= amount, "INSUFFICIENT BALANCE");
+
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+}
